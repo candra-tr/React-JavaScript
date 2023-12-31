d59e01b7ddb2eb57e53f61e0e4f11ddb7c6662ab
candra-tr <ctriatna@gmail.com>
2023-12-31 12:11:53 +0700

    add material components

--- 03. google-maps-travel/google-maps-travel/src/App.js
+++ 03. google-maps-travel/google-maps-travel/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {CssBaseLine, Grid} from '@material-ui/core';
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <CssBaseLine />
+      <Header />
+      <Grid Container Spacing={3} style={{ width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
