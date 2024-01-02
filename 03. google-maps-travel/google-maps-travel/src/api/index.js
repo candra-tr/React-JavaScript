9a95a75582a4529cca28f94b10610c21d1c5b705
candra-tr <ctriatna@gmail.com>
2024-01-02 14:17:12 +0700

    add indx.js in src

--- 03. google-maps-travel/google-maps-travel/src/api/index.js
+++ 03. google-maps-travel/google-maps-travel/src/api/index.js
@@ -1,1 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+const options = {
+      method: 'GET',
+      params: {
+        bl_latitude: '11.847676',
+        tr_latitude: '12.838442',
+        bl_longitude: '109.095887',
+        tr_longitude: '109.149359',
+      },
+      headers: {
+        'X-RapidAPI-Key': '54518c8466msh313123d400e6a55p1c0653jsn46b7c0dd7544',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    };
+    
+
+const getPlacesData = async () => {
+      try {
+         const {data: { data }} = await axios.get(URL, options);
+         return data;
+      }catch(error){
+         console.log(error);
+      }
+}
